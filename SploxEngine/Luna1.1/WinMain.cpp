2df8cfa676732432ee49e3aa32221dab2a201f43
Splox <mark.keenan.leblanc@gmail.com>
2021-05-17 22:18:18 -0400

    Changed to 1920x1080 default resolution, noticed flaw in rotational physics, thrusters act as if they themsleves never move, think of a camera spinning in a mount but its control thrusters are on a cage that is static to the surrondings. That is a complicated way to say that manuvering thruster behavior does is incorrectly not effected by ship orientation.

--- SploxEngine/Luna1.1/WinMain.cpp
+++ SploxEngine/Luna1.1/WinMain.cpp
@@ -2,6 +2,6 @@
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
 	DX12GraphicsEngine rendering_engine;
-	Win64App test(&rendering_engine, 1000, 1000, hInstance, nCmdShow);
+	Win64App test(&rendering_engine, 1920, 1080, hInstance, nCmdShow);
 	return test.Run();
 }
